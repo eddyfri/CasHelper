ca077152e946d25d23923183fe57472901820493
Simo416 <simos5@live.com>
2023-05-09 20:01:48 +0200

    try to do coloured circle

--- app/src/main/java/unipd/dei/cashelper/adapters/OutflowListAdapter.kt
+++ app/src/main/java/unipd/dei/cashelper/adapters/OutflowListAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
@@ -27,43 +28,43 @@ class OutflowListAdapter(private val itemByCategory: MutableMap<String, ArrayLis
         private val categoryItem: TextView = itemView.findViewById(R.id.category_item)
         private val totalItem: TextView = itemView.findViewById(R.id.total_item)
 
-        private val constraint = itemView.findViewById<ConstraintLayout>(R.id.constraint_incoming)
+        private val circle = itemView.findViewById<View>(R.id.cateogry_color)
         fun bind(categoryName: String, total: Double){
             categoryItem.text = categoryName
             totalItem.text = total.toString() + "€"
             when (categoryName) {
                 "Salario" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Salario))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Salario))
                 }
                 "Alimentari" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Alimentari))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Alimentari))
                 }
                 "Trasporti" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Trasporti))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Trasporti))
                 }
                 "Shopping" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Shopping))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Shopping))
                 }
                 "Viaggi" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Viaggi))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Viaggi))
                 }
                 "Bollette" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Bollette))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Bollette))
                 }
                 "Lavoro" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Lavoro))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Lavoro))
                 }
                 "Sport" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Sport))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Sport))
                 }
                 "Auto" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Auto))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Auto))
                 }
                 "Regali" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Regali))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Regali))
                 }
                 "Altro" -> {
-                    constraint.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Altro))
+                    circle.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.Altro))
                 }
             }
         }
